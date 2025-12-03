48bee64da13b43e60362aa9f721d3514967e766f
MariamE04 <elmirmariam16@gmail.com>
2025-12-03 19:18:01 +0100

    færdig

--- codelab/src/pages/Home.jsx
+++ codelab/src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import bt from "../assets/bt.jpg";
+import "../styles/home.css";
+
 function Home() {
   return (
     <div>
       <h1>Bornholmer Tours</h1>
-      <p>Velkommen til Bornholmer Tours – Din guide
-          til Bornholms bedste outdoor oplevelser
+      <p>
+        Velkommen til Bornholmer Tours – Din guide
+        til Bornholms bedste outdoor oplevelser
       </p>
+
+      <div className="bt">
+                <img src={bt} alt="bt" />
+              </div>
 
     </div>
   );
