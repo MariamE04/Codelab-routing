edde785aff8d850d8ddce40f03687ebd7b9343c4
MariamE04 <elmirmariam16@gmail.com>
2025-12-03 14:54:22 +0100

    tilføjet noget css

--- codelab/src/components/header/Header.jsx
+++ codelab/src/components/header/Header.jsx
@@ -1,41 +1,37 @@
 import { NavLink } from "react-router-dom";
 import "./header.css";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <nav className="header">
-      <ul>
-        <li>
-          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-            Home
-          </NavLink>
-        </li>
+    <header>
+      {/* Øverste header */}
+      <div className="top-header">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+          <h1>Bornholmer Tours</h1>
+        </div>
+        <NavLink to="/login" className="login-button">Login</NavLink>
+      </div>
 
-        <li>
-          <NavLink to="/travels" className={({ isActive }) => (isActive ? "active" : "")}>
-            Travels
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
-            About
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/admin" className={({ isActive }) => (isActive ? "active" : "")}>
-            Admin
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/login" className={({ isActive }) => (isActive ? "active" : "")}>
-            Login
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+      {/* Nederste navigation */}
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/travels" className={({ isActive }) => (isActive ? "active" : "")}>Travels</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin" className={({ isActive }) => (isActive ? "active" : "")}>Admin</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
